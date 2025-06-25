168198a15097d5f12489a7ecc6f34832fc40fe79
Luis Alejandro <luisalejandrosalguerotoloza@gmail.com>
2025-06-24 20:52:12 -0500

    hg

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-pip install -r requirements.txt
+# Install packages using pip directly
+python -m pip install --upgrade pip
+python -m pip install Flask==3.0.0 Werkzeug==3.0.1 gunicorn==22.0.0
