56b32da7eede690f69008db1c87491b684b139c2
Luis Alejandro <luisalejandrosalguerotoloza@gmail.com>
2025-06-24 20:55:58 -0500

    asd

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-# Install packages using pip directly
-python -m pip install --upgrade pip
-python -m pip install Flask==3.0.0 Werkzeug==3.0.1 gunicorn==22.0.0
+# Force installation in the current environment
+pip install --upgrade pip
+pip install Flask==3.0.0 Werkzeug==3.0.1 gunicorn==22.0.0 --force-reinstall
