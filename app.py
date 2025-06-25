5c371ba3a182af232ba6e14bfdcc410d7dc73bed
Luis Alejandro <luisalejandrosalguerotoloza@gmail.com>
2025-06-24 21:06:57 -0500

    as

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, abort
 import os
 
 app = Flask(__name__, 
@@ -14,10 +14,27 @@ def index():
 def serve_img(filename):
     return send_from_directory('img', filename)
 
+# Servir archivos de las subcarpetas específicas
+@app.route('/sub_t/<path:filepath>')
+def serve_sub_t(filepath):
+    return send_from_directory('sub_t', filepath)
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+# Servir archivos HTML desde templates
 @app.route('/<path:filename>')
 def serve_templates(filename):
-    # Serve other HTML files from templates folder
-    return send_from_directory('templates', filename)
+    try:
+        # Primero intentar desde templates
+        return send_from_directory('templates', filename)
+    except:
+        # Si no está en templates, buscar en otras carpetas
+        try:
+            return send_from_directory('.', filename)
+        except:
+            abort(404)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
